9ebad6943b6030a1a0d2305b6fc60e9613f99acf
Justin Bernard Boado <jbcboado@gmail.com>
2026-04-03 00:35:01 +0800

    Initial Github EC2 Action codes

--- helloJames.py
+++ helloJames.py
@@ -4,4 +4,5 @@ app = FastAPI()
 
 @app.get("/hello")
 def say_hello():
-    return {"message": "Hello, welcome James bading!"}
+    name = input("Enter your name please: ")
+    print("Hello " + name + " badingdong!!!")
