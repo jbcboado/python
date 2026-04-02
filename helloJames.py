17a71612ea42415dfff2d963ff7a5bbcc4a111e4
Justin Bernard Boado <jbcboado@gmail.com>
2026-04-03 00:45:15 +0800

    Initial Github EC2 Action codes

--- helloJames.py
+++ helloJames.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+import streamlit as st
 
 app = FastAPI()
 
 @app.get("/hello")
 def say_hello():
-    name = input("Enter your name please: ")
-    print("Hello " + name + " badingdong!!!")
+    user_input = st.text_input("Enter your name please:")
+    print("Hello " + user_input + " badingdong!!!")
